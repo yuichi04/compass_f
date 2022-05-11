eb7b69a09aa4c1b65b42cbbf01c9d106e028b34f
yuichi04 <sparklingengineer.yuya04gen@gmail.com>
2022-05-11 11:19:01 +0900

    Add .gitignore

--- src/lib/api/userAuth.ts
+++ src/lib/api/userAuth.ts
@@ -1,6 +1,10 @@
+import { SignUpParams } from "../../types/userTypes";
 import client from "./client";
 
 // Signup
+export const signUp = (params: SignUpParams) => {
+  return client.post(process.env.REACT_APP_USER_URL!, params);
+};
 // Login
 // logout
 // fetchUserState
